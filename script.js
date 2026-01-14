af2ac04d51bc524b6fd0f6fef44b60818fd758d6
Roblox-Groups <ogedajacob38@gmail.com>
2026-01-13 16:32:39 -0800

    Add click event to game cards for navigation

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function openGame(page) {
   window.location.href = page;
 }
+document.querySelectorAll(".game-card").forEach(card => {
+  card.addEventListener("click", () => {
+    window.location.href = "game.html";
+  });
+});
 
