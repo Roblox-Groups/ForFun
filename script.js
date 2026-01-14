1f92fa5a1e8d65f54c3117f6a5a73912b05dba4a
Roblox-Groups <ogedajacob38@gmail.com>
2026-01-13 18:25:38 -0800

    Refactor script.js to eliminate duplicate code
    Removed duplicate openGame function and event listeners.

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function openGame(page) {
-  window.location.href = page;
-}
-document.querySelectorAll(".game-card").forEach(card => {
-  card.addEventListener("click", () => {
-    window.location.href = "game.html";
-  });
-});
 
-// ADD GAME OPEN FUNCTION
-function openGame(page) {
-  window.location.href = page;
-}
